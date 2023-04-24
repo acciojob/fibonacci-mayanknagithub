f650fe5badbbc244742153cea1f4d46914009f79
Mayank Namdev <117892477+mayanknagithub@users.noreply.github.com>
2023-04-24 13:07:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-function fibonacci(num) {
-// your code here
+function fibonacci(nums){
+  let x = 0;
+  let y = 1;
+  let term = 1;
+  let sum = 0;
+  
+  while(term<=nums){
+      console.log(sum + " ");
+      x = y;
+      y = sum;
+      sum = x+y;
+      term = term + 1;
+  }
 }
+
+fibonacci();
 
 module.exports = fibonacci;
